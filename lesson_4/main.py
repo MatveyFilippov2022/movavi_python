367d6a911ed49493915223f1137ca52020c401dc
movavi_school <lavr.ilyk@yandex.ru>
2022-10-09 13:14:20 +0700

    урок прошёл 9 октября

---
+++ lesson_4/main.py
@@ -0,0 +1,47 @@
+# # x = "строка"
+# # x1 = "строка"
+# # x = 1 - они ничем не отличаются
+# # name = "Антон\nАнтон\nАнтон" первый способ разделять строки
+# # name =
+# # Строка 1
+# # Строка 2
+# # Строка 3
+# # print(name)
+# #
+# # st = "я большой"
+# # print(st.upper())
+# #
+# # st1 = "Я МАЛЕНЬКИЙ"
+# # print(st1.lower())
+# #
+# st2 = "я фраза и хочу начинатся с большой"
+# print(st2.capita
+#
+#
+# familia = input("Фамилия: ")
+# imya = ("Имя: ")
+# otchestvo = input("Отчество: ")
+#
+# print(familia, imya[0] + "." + otchestvo[0] + ".")
+# print(f"{familia} {imya[0]}. {otchestvo[0]}.")
+# st = input("Введи слово/фразу: ")
+# print(st.count("а")) # именно маленьких букв
+# print(st.count("а")) # все "а" и "А"
+#
+# phrase = input ("введи слова через запятую: ")
+# lst phrase.split(",")
+# удалить по значению = .remove ("строка")
+# удалить по индексу = pop(0)
+# lst.pop(0)
+# print(lst)
+#
+# phrase = input("Ваеди строчку:")
+# find = input("Что меняем:")
+# replace = input("На что меняем:")
+#
+# print(phrase.replace(find, replace))
+# #.replace() - метод для замены; find - то что меняем; replace - то НА что меняем
+
+phrase = input("Ваеди строчку:")
+print(phrase.replace("ё","е"))
+
