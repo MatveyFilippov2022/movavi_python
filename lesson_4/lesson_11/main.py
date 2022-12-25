25675cb52128170d0832d4078d60c18fb6949a8b
midix <bodya.podkopaev@yandex.ru>
2022-12-25 13:08:24 +0700

    урок 25 декабря

---
+++ lesson_4/lesson_11/main.py
@@ -0,0 +1,72 @@
+# множества
+# 1) Повторение исключены
+# 2) Внутри только неизменяемые типы данных
+# 3) Неупорядоченное содержимое
+# 4) Пустое множество - только set()
+
+# словари
+# 1) Пустой словарь - либо dict(), либо {}
+# 2) {Ключ: значение} В качестве ключа допускаются любые тип.д.
+
+
+# phrase = "Я всегда я, когда я есть я."
+# symbols = list("!.,?") # нежелательные знаки
+# phrase_cleared = "" # будущая очищенная строка
+#
+# for dfg in phrase: # побуквенный перебор строчки
+# if dfg not in symbols:
+# phrase_cleared += dfg
+#
+# l = phrase_cleared.split(" ")
+# d = {} asdsadasd
+#
+# for element in l:adadasda
+# if element not in d:adasdasdad
+# d[element] = 1dadsadasda
+# else:
+# print(d)
+# d[element] += 1
+#
+# # pocupki = {"Чипсы": 78,
+# # "Кирирешки": 13,
+# # "Яйца": 70,
+# # "Мега Антон": 999}
+# # s = 0
+# #
+# # for i in pokuoki:
+# # phrase = "Я всегда я, когда я есть я."
+# # symbols = list("!.,?") # нежелательные знаки
+# # phrase_cleared = "" # будущая очищенная строка
+# #
+# # for dfg in phrase: # побуквенный перебор строчки
+# # if dfg not in symbols:
+# # phrase_cleared += dfg
+# #
+# # l = phrase_cleared.split(" ")
+# # d = {}
+# #
+# # for element in l:
+# # if element not in d:
+# # d[element] =
+# # else:
+# # d[element] += 1
+# # print(d)
+# phrase = "Я всегда я, когда я есть я.".lower()
+# symbols = list("!.,?") # нежелательные знаки
+# phrase_cleared = "" # будущая очищенная строка
+#
+# for dfg in phrase: # побуквенный перебор строчки
+#     if dfg not in symbols:
+#         phrase_cleared += dfg
+#
+# l = phrase_cleared.split(" ")
+# d = {}
+#
+# for element in l:
+#     if element not in d:
+#         d[element] = 1
+#     else:
+#         d[element] += 1
+# pri
+
+
